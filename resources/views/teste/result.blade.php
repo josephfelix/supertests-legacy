caca0581bc365c7ade2503a30b2ccd8680033ef7
Joseph Felix <joseph0xfff@gmail.com>
2017-04-16 23:23:48 -0300

    Revert "Enviado banner de 300x250"
    This reverts commit 7a7662be06e82c7167cb9d900e958ce8fc2e66d7.

--- resources/views/teste/result.blade.php
+++ resources/views/teste/result.blade.php
@@ -55,11 +55,12 @@
                     <hr class="hr-teste-single" />
 
                     <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-                    <!-- 300x250sitetesteweb.com -->
+                    <!-- responsivositetesteweb.com -->
                     <ins class="adsbygoogle"
-                         style="display:inline-block;width:300px;height:250px"
+                         style="display:block"
                          data-ad-client="ca-pub-9156209932510160"
-                         data-ad-slot="2643075533"></ins>
+                         data-ad-slot="8689609135"
+                         data-ad-format="auto"></ins>
                     <script>
                         (adsbygoogle = window.adsbygoogle || []).push({});
                     </script>
